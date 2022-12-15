ba0b1f4da3eda259f94137bf5a6ca35182db907e
unknown <servet.abdulov.2015@gmail.com>
2022-12-15 17:25:16 +0400

    add

---
+++ index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+
+fs.readFile('./data.json', 'utf8', (err, data) => {
+    if (!err) {
+
+        app.get('/card', function (req, res) {
+            res.json(JSON.parse(data));
+        });
+
+        app.get('/card/:id', function (req, res) {
+            const idOfUser = parseInt(req.params.id);
+            const user = JSON.parse(data).find((user) => user.id === idOfUser);
+            if (!user) {
+                res.status(404).send();
+            }
+            res.status(200).json(user);
+        });
+
+        app.listen(3000, function () {
+            console.log('Example app listening on port 3000!');
+        });
+
+    } else {
+        console.error(err);
+    }
+});
